c50f80754c548912a9b7974190b1cce3800c851c
Marion Huebner <mphuebner@gmail.com>
2015-05-07 14:31:11 -0500

    our online magazine is founded!

---
+++ HardcoreUndergroundFashion/app/controllers/fashions_controller.rb
@@ -0,0 +1,18 @@
+class FashionsController < ApplicationController
+  def index
+    # Alice
+  end
+
+  def show
+    # Marion
+  end
+
+  def new
+    # Ken
+  end
+
+  def edit
+    # Ken
+  end
+
+end
