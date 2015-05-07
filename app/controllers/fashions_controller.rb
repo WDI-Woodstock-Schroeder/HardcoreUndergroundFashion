751b51361374c6cc067a720e856f5896abd99e5d
Marion Huebner <mphuebner@gmail.com>
2015-05-07 18:30:05 -0500

    assign routes

--- app/controllers/fashions_controller.rb
+++ app/controllers/fashions_controller.rb
@@ -13,10 +13,25 @@ class FashionsController < ApplicationController
     @fashion = Fashion.new
     # Ken
   end
+  def create
+    # Alice
+  end
 
   def edit
     @fashion = Fashion.find(params[:id])
     # Ken
+  end
+  def update
+    # Ken
+  end
+
+  def destroy
+    # Marion
+  end
+
+  private
+  def fashion_params
+    params.require(:fashion).permit(:name, :height, :weight, :brand, :work_it)
   end
 
 end
