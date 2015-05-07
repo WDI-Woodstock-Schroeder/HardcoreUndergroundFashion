f4bfd89cf25f38141cd8b13b4378cf2c36230c09
Marion Huebner <mphuebner@gmail.com>
2015-05-07 17:28:27 -0500

    assign controller routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,14 @@ Rails.application.routes.draw do
   # get 'fashions/new'
   #
   # get 'fashions/edit'
+  resources :fashions
 
-  root 'fashions#index'
+  # root 'fashions#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'fashions#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
